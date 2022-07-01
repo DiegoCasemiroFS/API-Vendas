c92ee10b1a34e0a67f6862c0bb12b7c7c6770a49
Diego Casemiro <107040723+DiegoCasemiroFS@users.noreply.github.com>
2022-07-01 19:43:09 -0300

    primeiro commit

---
+++ src/test/java/DiegoCasemiroFS/com/github/APIVendas/java/ApplicationTests.java
@@ -0,0 +1,13 @@
+package DiegoCasemiroFS.com.github.APIVendas.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
